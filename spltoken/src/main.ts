50b85960dccb157732c0b1c39fcb6ed767999d1d
IzuKripto <kjrwesley21@gmail.com>
2024-07-27 05:06:09 +0100

    registering wall keypair as transactions signer

--- spltoken/src/main.ts
+++ spltoken/src/main.ts
@@ -2,3 +2,14 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { userKeypair } from "./helpers";
 
 const umi = createUmi ('https://api.devnet.solana.com');
+
+import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { keypairIdentity } from "@metaplex-foundation/umi";
+//import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+//import { userKeypair } from "./helpers";
+
+//const umi = createUmi ('https://api.devnet.solana.com');
+
+const keypair = umi.eddsa.createKeypairFromSecretKey (userKeypair.secretKey);
+
+umi.use (keypairIdentity(keypair)).use (mplTokenMetadata())
