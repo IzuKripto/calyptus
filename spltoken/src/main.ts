128fdd8c6465a1ec9c0e32689ac15137f87dd776
IzuKripto <kjrwesley21@gmail.com>
2024-07-27 05:27:37 +0100

    function to mint tokens using MintV1

--- spltoken/src/main.ts
+++ spltoken/src/main.ts
@@ -3,7 +3,7 @@ import { userKeypair } from "./helpers";
 
 const umi = createUmi ('https://api.devnet.solana.com');
 
-import { createV1, mplTokenMetadata, TokenStandard } from "@metaplex-foundation/mpl-token-metadata";
+import { createV1, mintV1, mplTokenMetadata, TokenStandard } from "@metaplex-foundation/mpl-token-metadata";
 import { generateSigner, keypairIdentity, percentAmount } from "@metaplex-foundation/umi";
 //import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 //import { userKeypair } from "./helpers";
@@ -32,4 +32,14 @@ async function createMetadataDetails() {
         decimals: 9,
         tokenStandard: TokenStandard.Fungible,
     }).sendAndConfirm (umi)
+}
+
+async function mintToken() {
+    await mintV1 (umi, {
+        mint: mint.publicKey,
+        authority: umi.identity,
+        amount: 10_000,
+        tokenOwner: umi.identity.publicKey,
+        tokenStandard: TokenStandard.Fungible,
+    }).sendAndConfirm(umi)
 }
