0482ee02e55a0caf25e0d21d262b202de2500366
IzuKripto <kjrwesley21@gmail.com>
2024-07-27 05:22:28 +0100

    created metadata accout using createV1 method

--- spltoken/src/main.ts
+++ spltoken/src/main.ts
@@ -3,8 +3,8 @@ import { userKeypair } from "./helpers";
 
 const umi = createUmi ('https://api.devnet.solana.com');
 
-import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
-import { keypairIdentity } from "@metaplex-foundation/umi";
+import { createV1, mplTokenMetadata, TokenStandard } from "@metaplex-foundation/mpl-token-metadata";
+import { generateSigner, keypairIdentity, percentAmount } from "@metaplex-foundation/umi";
 //import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 //import { userKeypair } from "./helpers";
 
@@ -13,3 +13,23 @@ import { keypairIdentity } from "@metaplex-foundation/umi";
 const keypair = umi.eddsa.createKeypairFromSecretKey (userKeypair.secretKey);
 
 umi.use (keypairIdentity(keypair)).use (mplTokenMetadata())
+
+const metadata = {
+    name: "Solana Gold",
+    symbol: "SOLGOL",
+    uri: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json",
+};
+
+const mint = generateSigner (umi);
+async function createMetadataDetails() {
+    await createV1 (umi, {
+        mint,
+        authority: umi.identity,
+        name: metadata.name,
+        symbol: metadata.symbol,
+        uri: metadata.uri,
+        sellerFeeBasisPoints: percentAmount(0),
+        decimals: 9,
+        tokenStandard: TokenStandard.Fungible,
+    }).sendAndConfirm (umi)
+}
